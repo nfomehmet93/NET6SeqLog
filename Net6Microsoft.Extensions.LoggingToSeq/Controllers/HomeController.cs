0df8d4ae4527bd24c3d872376b45915a150fd0b7
Mehmet uluekmekçi <mehmetaliuluekmekci@hotmail.com>
2022-06-04 04:20:27 +0300

    Add project files.

---
+++ Net6Microsoft.Extensions.LoggingToSeq/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Net6Microsoft.Extensions.LoggingToSeq.Models;
+using System.Diagnostics;
+
+namespace Net6Microsoft.Extensions.LoggingToSeq.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+            string firstLog = "First Log!";
+            _logger.LogInformation("Hello {firstLog}", firstLog);
+        }
+
+        public IActionResult Index()
+        {
+            _logger.LogError("Hello! Index LogError");
+            _logger.LogWarning("Hello! Index LogWarning");
+            _logger.LogTrace("Hello! Index LogTrace");
+            _logger.LogInformation("Hello! Index LogInformation");
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Net6SeriLogToSeq/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Net6SeriLogToSeq.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
